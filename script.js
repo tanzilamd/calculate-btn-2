dc391eca6760f464427ac6f4fca3edba5217b304
tanzilamd <mdtanzil858@gmail.com>
2021-07-08 13:34:46 +0600

    function added

--- script.js
+++ script.js
@@ -1,42 +1,87 @@
-function cal(num1, num2) {
-    console.log(`Sum = ${num1 + num2}`);
-    console.log(`Sub = ${num1 - num2}`);
-    console.log(`Mul = ${num1 * num2}`);
-    console.log(`Div = ${num1 / num2}`);
-    console.log(`Mod = ${num1 % num2}`);
+// function cal(num1, num2) {
+//     console.log(`Sum = ${num1 + num2}`);
+//     console.log(`Sub = ${num1 - num2}`);
+//     console.log(`Mul = ${num1 * num2}`);
+//     console.log(`Div = ${num1 / num2}`);
+//     console.log(`Mod = ${num1 % num2}`);
+
+//     document.getElementById("myList").innerHTML = "";
+//     var node = document.createElement("LI");
+//     var textNode = document.createTextNode(`(${num1}, ${num2})`);
+//     node.appendChild(textNode);
+//     document.getElementById("myList").appendChild(node);
+
+//     var node = document.createElement("LI");
+//     var textNode = document.createTextNode(`Sum = ${num1 + num2}`);
+//     node.appendChild(textNode);
+//     document.getElementById("myList").appendChild(node);
+
+//     var node = document.createElement("LI");
+//     var textNode = document.createTextNode(`Sub = ${num1 - num2}`);
+//     node.appendChild(textNode);
+//     document.getElementById("myList").appendChild(node);
+
+//     var node = document.createElement("LI");
+//     var textNode = document.createTextNode(`Mul = ${num1 * num2}`);
+//     node.appendChild(textNode);
+//     document.getElementById("myList").appendChild(node);
+
+//     var node = document.createElement("LI");
+//     var textNode = document.createTextNode(`Div = ${num1 / num2}`);
+//     node.appendChild(textNode);
+//     document.getElementById("myList").appendChild(node);
+
+//     var node = document.createElement("LI");
+//     var textNode = document.createTextNode(`Mod = ${num1 % num2}`);
+//     node.appendChild(textNode);
+//     document.getElementById("myList").appendChild(node);
+    
+//     document.getElementById("num1").value = "";
+//     document.getElementById("num2").value = "";
+// }
+
+function sum(num1, num2) {
 
     document.getElementById("myList").innerHTML = "";
-    var node = document.createElement("LI");
-    var textNode = document.createTextNode(`(${num1}, ${num2})`);
-    node.appendChild(textNode);
-    document.getElementById("myList").appendChild(node);
-
     var node = document.createElement("LI");
     var textNode = document.createTextNode(`Sum = ${num1 + num2}`);
     node.appendChild(textNode);
     document.getElementById("myList").appendChild(node);
+}
 
+function sub(num1, num2) {
+
+    document.getElementById("myList").innerHTML = "";
     var node = document.createElement("LI");
     var textNode = document.createTextNode(`Sub = ${num1 - num2}`);
     node.appendChild(textNode);
     document.getElementById("myList").appendChild(node);
+}
 
+function mul(num1, num2) {
+
+    document.getElementById("myList").innerHTML = "";
     var node = document.createElement("LI");
     var textNode = document.createTextNode(`Mul = ${num1 * num2}`);
     node.appendChild(textNode);
     document.getElementById("myList").appendChild(node);
+}
 
+function div(num1, num2) {
+
+    document.getElementById("myList").innerHTML = "";
     var node = document.createElement("LI");
     var textNode = document.createTextNode(`Div = ${num1 / num2}`);
     node.appendChild(textNode);
     document.getElementById("myList").appendChild(node);
+}
 
+function mod(num1, num2) {
+
+    document.getElementById("myList").innerHTML = "";
     var node = document.createElement("LI");
     var textNode = document.createTextNode(`Mod = ${num1 % num2}`);
     node.appendChild(textNode);
     document.getElementById("myList").appendChild(node);
-    
-    document.getElementById("num1").value = "";
-    document.getElementById("num2").value = "";
 }
 
